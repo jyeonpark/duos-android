e41b45c54373072c814d7ffc9163c520bc13d6d0
sh1mj1 <7wlgns@gmail.com>
2022-01-14 13:43:29 +0900

    220114-FIRST-MERGE 된 것 PULL

---
+++ app/build/tmp/kapt3/stubs/debug/com/example/duos/data/entities/Review.java
@@ -0,0 +1,93 @@
+package com.example.duos.data.entities;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000 \n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\b\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0011\n\u0002\u0010\u000b\n\u0002\b\u0004\b\u0086\b\u0018\u00002\u00020\u0001B/\u0012\n\b\u0002\u0010\u0002\u001a\u0004\u0018\u00010\u0003\u0012\b\b\u0002\u0010\u0004\u001a\u00020\u0005\u0012\b\b\u0002\u0010\u0006\u001a\u00020\u0005\u0012\b\b\u0002\u0010\u0007\u001a\u00020\u0005\u00a2\u0006\u0002\u0010\bJ\u0010\u0010\u0010\u001a\u0004\u0018\u00010\u0003H\u00c6\u0003\u00a2\u0006\u0002\u0010\fJ\t\u0010\u0011\u001a\u00020\u0005H\u00c6\u0003J\t\u0010\u0012\u001a\u00020\u0005H\u00c6\u0003J\t\u0010\u0013\u001a\u00020\u0005H\u00c6\u0003J8\u0010\u0014\u001a\u00020\u00002\n\b\u0002\u0010\u0002\u001a\u0004\u0018\u00010\u00032\b\b\u0002\u0010\u0004\u001a\u00020\u00052\b\b\u0002\u0010\u0006\u001a\u00020\u00052\b\b\u0002\u0010\u0007\u001a\u00020\u0005H\u00c6\u0001\u00a2\u0006\u0002\u0010\u0015J\u0013\u0010\u0016\u001a\u00020\u00172\b\u0010\u0018\u001a\u0004\u0018\u00010\u0001H\u00d6\u0003J\t\u0010\u0019\u001a\u00020\u0003H\u00d6\u0001J\t\u0010\u001a\u001a\u00020\u0005H\u00d6\u0001R\u0011\u0010\u0004\u001a\u00020\u0005\u00a2\u0006\b\n\u0000\u001a\u0004\b\t\u0010\nR\u0015\u0010\u0002\u001a\u0004\u0018\u00010\u0003\u00a2\u0006\n\n\u0002\u0010\r\u001a\u0004\b\u000b\u0010\fR\u0011\u0010\u0006\u001a\u00020\u0005\u00a2\u0006\b\n\u0000\u001a\u0004\b\u000e\u0010\nR\u0011\u0010\u0007\u001a\u00020\u0005\u00a2\u0006\b\n\u0000\u001a\u0004\b\u000f\u0010\n\u00a8\u0006\u001b"}, d2 = {"Lcom/example/duos/data/entities/Review;", "", "profileImg", "", "playerGrade", "", "profileNickname", "review_content_tv", "(Ljava/lang/Integer;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V", "getPlayerGrade", "()Ljava/lang/String;", "getProfileImg", "()Ljava/lang/Integer;", "Ljava/lang/Integer;", "getProfileNickname", "getReview_content_tv", "component1", "component2", "component3", "component4", "copy", "(Ljava/lang/Integer;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)Lcom/example/duos/data/entities/Review;", "equals", "", "other", "hashCode", "toString", "app_debug"})
+public final class Review {
+    @org.jetbrains.annotations.Nullable()
+    private final java.lang.Integer profileImg = null;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String playerGrade = null;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String profileNickname = null;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String review_content_tv = null;
+    
+    @org.jetbrains.annotations.NotNull()
+    public final com.example.duos.data.entities.Review copy(@org.jetbrains.annotations.Nullable()
+    java.lang.Integer profileImg, @org.jetbrains.annotations.NotNull()
+    java.lang.String playerGrade, @org.jetbrains.annotations.NotNull()
+    java.lang.String profileNickname, @org.jetbrains.annotations.NotNull()
+    java.lang.String review_content_tv) {
+        return null;
+    }
+    
+    @java.lang.Override()
+    public boolean equals(@org.jetbrains.annotations.Nullable()
+    java.lang.Object other) {
+        return false;
+    }
+    
+    @java.lang.Override()
+    public int hashCode() {
+        return 0;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    @java.lang.Override()
+    public java.lang.String toString() {
+        return null;
+    }
+    
+    public Review() {
+        super();
+    }
+    
+    public Review(@org.jetbrains.annotations.Nullable()
+    java.lang.Integer profileImg, @org.jetbrains.annotations.NotNull()
+    java.lang.String playerGrade, @org.jetbrains.annotations.NotNull()
+    java.lang.String profileNickname, @org.jetbrains.annotations.NotNull()
+    java.lang.String review_content_tv) {
+        super();
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    public final java.lang.Integer component1() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    public final java.lang.Integer getProfileImg() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component2() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getPlayerGrade() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component3() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getProfileNickname() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component4() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getReview_content_tv() {
+        return null;
+    }
+}
