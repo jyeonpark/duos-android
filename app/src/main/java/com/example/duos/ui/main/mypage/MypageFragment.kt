a0ffb431a444dd386e45afd253a6b3ef76534d0c
sh1mj1 <7wlgns@gmail.com>
2022-01-18 22:01:50 +0900

    마이페이지와 연결

--- app/src/main/java/com/example/duos/ui/main/mypage/MypageFragment.kt
+++ app/src/main/java/com/example/duos/ui/main/mypage/MypageFragment.kt
@@ -4,8 +4,8 @@ import android.content.Intent
 import com.example.duos.databinding.FragmentMypageBinding
 import com.example.duos.ui.BaseFragment
 import com.example.duos.ui.main.mypage.customerservice.CustomerServiceActivity
-import com.example.duos.ui.main.mypage.myprofile.MyProfileActivity
 import com.example.duos.ui.main.mypage.lastpromise.PreviousGameActivity
+import com.example.duos.ui.main.mypage.myprofile.MyProfileActivity
 import com.example.duos.ui.main.mypage.notion.NotionActivity
 import com.example.duos.ui.main.mypage.setup.SetupActivity
 
@@ -14,31 +14,31 @@ class MypageFragment() : BaseFragment<FragmentMypageBinding>(FragmentMypageBindi
     override fun initAfterBinding() {
 
 //        나의 프로필 클릭 리스너
-        binding.myPageTv.setOnClickListener {
+        binding.myProfileHomeConstraintLayoutCl.setOnClickListener {
             val intent = Intent(activity, MyProfileActivity::class.java)
             startActivity(intent)
         }
 
 //        지난 약속 클릭 리스너
-        binding.myPageTv2.setOnClickListener {
+        binding.myPageLastAppointmentTextTv.setOnClickListener {
             val intent = Intent(activity, PreviousGameActivity::class.java)
             startActivity(intent)
         }
 
 //        공지 사항 클릭 리스너
-        binding.myPageTv3.setOnClickListener {
+        binding.myPageNoticeTextTv.setOnClickListener {
             val intent = Intent(activity, NotionActivity::class.java)
             startActivity(intent)
         }
 
 //        고객 센터 클릭 리스너
-        binding.myPageTv4.setOnClickListener {
+        binding.myPageCustomerServiceCenterTextTv.setOnClickListener {
             val intent = Intent(activity, CustomerServiceActivity::class.java)
             startActivity(intent)
         }
 
 //       설정 클릭 리스너
-        binding.myPageTv5.setOnClickListener {
+        binding.myPageSetTextTv.setOnClickListener {
             val intent = Intent(activity, SetupActivity::class.java)
             startActivity(intent)
         }
